ba58bb772cfc28c4cd0275547433b62c3d4a702e
PureFunctor <purefunctor@gmail.com>
2020-12-03 17:16:27 +0800

    Add types for functions

--- src/App.purs
+++ src/App.purs
@@ -8,10 +8,19 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
+type State = Int
+
 data Action = Increment | Decrement
 
 css = HP.class_ <<< HH.ClassName
 
+-- The HTML type says that the Component manipulates the DOM, the
+-- the `query` type represents how parent components communicate
+-- with child components, the `input` type specifies the data that
+-- the component accepts, the `output` type specifies the data that
+-- the component sends to the parent, and `m` being used to run
+-- effects.
+component :: forall query input output m. H.Component HH.HTML query input output m
 component =
   H.mkComponent
     { initialState
@@ -19,8 +28,14 @@ component =
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
 
+-- The type of the input does not matter as we're not handling it.
+initialState :: forall input. input -> State
 initialState _ = 0
 
+-- ComponentHTML is a more specialized type of HTML, specifically
+-- meant for use in components. `()` is used in place to describe
+-- child components with `m` being a monadic type for use in effects.
+render :: forall m. State -> H.ComponentHTML Action () m
 render state =
   HH.div [ css "hero is-primary is-fullheight" ]
   [ HH.div [ css "hero-body" ]
@@ -34,6 +49,15 @@ render state =
     ]
   ]
 
+-- HalogenM is the main Monad Transformer type used for handling
+-- component state while also composing it together with other
+-- monads through `m`. It has to know the type of the `State`
+-- being encapsulated, the type of the `Action` being received,
+-- any child components signified by `()`, and the type of the
+-- `output` to be communicated to any parent component if any.
+-- This is also typically parameterized by `Unit` as state
+-- modifications do not emit any resulting values.
+handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
 handleAction = case _ of
   Decrement ->
     H.modify_ \state -> state - 1
